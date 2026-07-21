0246c6611f05ce625117dbee6a447330d26ce0cc
SDobyS <195779301+SDobyS@users.noreply.github.com>
2026-07-21 04:31:35 +0300

    feat: add colors, 'q' exit and invalid op handling

--- src/main.asm
+++ src/main.asm
@@ -2,27 +2,28 @@ global _start
 
 section .data 
     ; title
-    title:          db '========================================', 10
-                    db '        ASM SYSCALL CALCULATOR          ', 10
-                    db '========================================', 10
-                    db 10
+    title:          db             '========================================', 10
+                    db 27, "[32m", '        ASM SYSCALL CALCULATOR          ', 27, "[0m", 10
+                    db             '========================================', 10
+                    db 10, 27, "[36m",
                     db '[1] Addition                            ', 10
                     db '[2] Subtraction                         ', 10
                     db '[3] Multiplication                      ', 10
                     db '[4] Division                            ', 10
-                    db 10
+                    db '[Q] Exit                                ', 10
+                    db 10, 27, "[0m"
     len_title:      equ $ - title
 
     ; num1 
-    prompt1:        db 'Enter your first number: '
+    prompt1:        db 27, "[33m", 'Enter your first number: ', 27, "[0m"
     len_prompt1:    equ $ - prompt1
 
     ; num2
-    prompt2:        db 'Enter your second number: '
+    prompt2:        db 27, "[33m", 'Enter your second number: ', 27, "[0m"
     len_prompt2:    equ $ - prompt2
 
     ; operatiom
-    prompt3:        db 'Enter your operation: '
+    prompt3:        db 27, "[33m", 'Enter your operation: ', 27, "[0m"
     len_prompt3:    equ $ - prompt3
 
     OP_ADD:         equ 1
@@ -30,11 +31,17 @@ section .data
     OP_MUL:         equ 3
     OP_DIV:         equ 4
 
-    res_msg:        db 'Result: '
+    res_msg:        db 27, "[32m", 'Result: ', 27, "[0m"
     len_res:        equ $ - res_msg
 
-    err_msg:        db "Cannot be divided by 0!", 10
+    err_msg:        db 27, "[31m", 'Error: Cannot be divided by 0!', 27, "[0m", 10
     len_err:        equ $ - err_msg
+
+    err_op_msg:     db 27, "[31m", "Error: Invalid operation!", 27, "[0m", 10
+    len_err_op:     equ $ - err_op_msg
+
+    exit_msg:       db 27, "[90m", 'Exit...', 27, "[0m", 10
+    len_exit:       equ $ - exit_msg
 
     newline:        db 10
 
@@ -71,6 +78,8 @@ _start:
     ; input num1 
     mov rsi, buf_num1
     call input_str
+    call check_exit
+
     mov rsi, buf_num1
     call str_to_int
     mov [num1], rax
@@ -84,6 +93,8 @@ _start:
     ; input num2 
     mov rsi, buf_num2
     call input_str
+    call check_exit
+
     mov rsi, buf_num2
     call str_to_int
     mov [num2], rax
@@ -97,6 +108,8 @@ _start:
     ; input oper
     mov rsi, buf_oper
     call input_str
+    call check_exit
+    
     mov rsi, buf_oper
     call str_to_int
     mov [oper], rax
@@ -111,7 +124,12 @@ _start:
     je .case_mul
     cmp rax, OP_DIV
     je .case_div
-
+    
+    ; check
+    mov rsi, err_op_msg
+    mov rdx, len_err_op
+    call print_str
+    
     ; exit
     jmp exit
 
@@ -183,7 +201,17 @@ print_str:
     syscall
     ret
 
+check_exit:
+    cmp byte [rsi], 'q'
+    je exit
+    cmp byte [rsi], 'Q'
+    je exit
+    ret
+
 exit:
+    mov rsi, exit_msg
+    mov rdx, len_exit
+    call print_str
     mov rax, 60 
     mov rdi, 0
     syscall
